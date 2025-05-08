0a11012bde6e64edd68d30e9a959075e96b044d5
Andreicomia11-12 <andreicomia7@gmail.com>
2025-05-08 11:34:14 +0800

    safasa

---
+++ README.md
@@ -0,0 +1,1 @@
+# SK-INSIGHT-REACT-
---
+++ reactProject/src/App.jsx
@@ -0,0 +1,64 @@
+function App() {
+  return (
+    <>
+      <div className="grid h-screen grid-rows-[34%_70%]">
+        <div className="bg-[#0A2C59] login relative">
+          <div className="logo-div w-full flex justify-center items-center mt-5">
+            <img src="./assets/Logo.png" className="logo" alt="" />
+          </div>
+          <div className="flex justify-center items-center mt-2 text-center w-full">
+            <p className="w-46 text-white font-medium">
+              Sangguniang Kabataan Puting Bato West Calaca City
+            </p>
+          </div>
+
+          {/* Positioned SVG image at the bottom */}
+          <img
+            src="./assets/Shape.svg"
+            className="loginframe absolute bottom-0 left-0 w-full h-[60px] -mb-12 object-cover md:none"
+            alt=""
+          />
+        </div>
+
+        <div className="bg-[#FFFFFF] mt-10 flex justify-center align-items-center">
+          <div className="shadow-lg h-70 w-70 mt-10 rounded-md md:w-h-100">
+            <div className="w-full flex justify-center align-items-center mt-4">
+              <h2 className="font-sans">Sign In</h2>
+            </div>
+            <div className="w-full flex justify-center align-items-center mt-4">
+              <form action="home.html" className="font-sans" method="POST">
+                <input
+                  type="text"
+                  className="username p-2 bg-[#f5f5f4] p-xm rounded-sm"
+                  placeholder="Username:"
+                /><br />
+                <br />
+                <input
+                  type="password"
+                  className="Password p-2 bg-[#f5f5f4] p-2 rounded-sm mb-4"
+                  placeholder="Password:"
+                /><br />
+                <label className="text-xs">
+                  <input type="checkbox" name="remember" />
+                  Remember Me
+                </label>
+                <button
+                  type="submit"
+                  className="w-full h-9 bg-[#0A2C59] text-white rounded-sm mt-2"
+                >
+                  Sign In
+                </button>
+                <p className="text-xs mt-2 text-black">
+                  Don't have an Account? <span className="text-[#0A2C59]">Sign Up</span>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
